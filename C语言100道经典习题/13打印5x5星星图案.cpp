90ee7a55dae997e4f977223a1e3bbceef54277c4
Miss-Lyan <31406658+Miss-Lyan@users.noreply.github.com>
2019-08-15 21:50:41 +0800

    Add files via upload

---
+++ C语言100道经典习题/13打印5x5星星图案.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    for(int i=0;i<=4;i++)
+    {
+       for(int n=0;n<=4;n++)
+          printf("*");
+       printf("\n");
+    }
+    return 0;
+}
---
+++ C语言100道经典习题/14判断输入日期是该年的第几天.cpp
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+    int year,mouth,day,today,sum=0,flag=0;
+    printf("请输入“年,月,日”\n");
+    scanf("%d,%d,%d",&year,&mouth,&day);
+    mouth=mouth-1;
+    for(;mouth>=1;mouth--)
+    {
+        if(year%400==0||(year%4==0&&year%100!=0))
+            flag=1;
+        if(mouth==4||mouth==6||mouth==9||mouth==11)
+            sum+=30;
+        else if(mouth==2&&flag==1)
+            sum+=29;
+        else if(mouth==2&&flag==0)
+            sum+=28;
+        else
+            sum+=31;
+    }
+    printf("\n是%d年的第%d天",year,sum+day);
+    return 0;
+}
